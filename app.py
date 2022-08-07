9d5d4aea6c4ef77d83d98613d618649f857ec498
Lokak07 <63875574+Lokak07@users.noreply.github.com>
2022-08-07 23:42:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ uploaded_file = st.sidebar.file_uploader("Choose a file")
 if uploaded_file is not None:
      bytes_data = uploaded_file.getvalue()
      data = bytes_data.decode("utf-8")
-     df = preprocessor.preprocess(data)
+     df = preprocess.preprocess(data)
 
      st.dataframe(df)
 
